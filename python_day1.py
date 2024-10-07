95305be29cd024557168d91cd585e5a321f35903
Lucy Wang <102928676+Sooshieroll@users.noreply.github.com>
2024-10-06 18:09:10 -0700

    first day

---
+++ python_day1.py
@@ -0,0 +1,10 @@
+# print("Hello " + input("What is your name?\n") + "!")
+
+# name = "Lucy"
+# length = len(name)
+# print(length)
+
+print("Welcome to the Band Name Generator!")
+city = input("Which city did you grow up in?\n")
+pet = input("What was your first pets name?\n")
+print("Your band name is " + city + " " + pet + "!")
